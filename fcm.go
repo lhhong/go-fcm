962bff14bcd7e0c7cf60bb17cd9ad1d5313f93aa
lhhong <heok.hong.low@gmail.com>
2018-12-31 15:43:55 +0800

    Complete basic implementation

--- fcm.go
+++ fcm.go
@@ -1,5 +1,11 @@
 package fcm
 
+import (
+	"log"
+	"math"
+	"math/rand"
+)
+
 // Interface Define the operations within values
 type Interface interface {
 	Multiply(weight float64) Interface
@@ -8,22 +14,76 @@ type Interface interface {
 }
 
 // Cluster Perform FCM with randomly initialized centroids
-func Cluster(vals []Interface, fuzziness float64, numCentroids int) ([]Interface, [][]float64) {
+func Cluster(vals []Interface, fuzziness float64, epsilon float64, numCentroids int) ([]Interface, [][]float64) {
 
 	centroids := make([]Interface, 0, numCentroids)
 
-	return ClusterWithInitialCentroids(vals, fuzziness, centroids)
-}
+	// TODO: Use map generation if ratio of numCentroids/len(val) is too small
+	randIndices := rand.Perm(len(vals))[:numCentroids]
 
-// ClusterWithInitialCentroids Perform FCM with manually initialized centroids
-func ClusterWithInitialCentroids(vals []Interface, fuzziness float64, initCentroids []Interface) ([]Interface, [][]float64) {
-
-	centroids := make([]Interface, 0, len(initCentroids))
-	copy(centroids, initCentroids)
-	clusterWeights := make([][]float64, len(centroids))
-	for i := 0; i < len(centroids); i++ {
-		clusterWeights = append(clusterWeights, make([]float64, len(vals)))
+	for _, i := range randIndices {
+		centroids = append(centroids, vals[i])
 	}
 
-	return centroids, clusterWeights
+	return centroids, ClusterGivenCentroids(vals, fuzziness, epsilon, centroids)
+}
+
+// ClusterGivenCentroids Perform FCM with manually initialized centroids
+func ClusterGivenCentroids(vals []Interface, fuzziness float64, epsilon float64, centroids []Interface) [][]float64 {
+
+	clusterWeights := make([][]float64, len(centroids))
+	for i := 0; i < len(centroids); i++ {
+		clusterWeights[i] = make([]float64, len(vals))
+	}
+
+	for evaluateWeights(vals, fuzziness, centroids, clusterWeights) > epsilon {
+		recenter(vals, fuzziness, centroids, clusterWeights)
+	}
+
+	return clusterWeights
+}
+
+func evaluateWeights(vals []Interface, fuzziness float64, centroids []Interface, clusterWeights [][]float64) float64 {
+
+	squareSum := 0.0
+	for j, c := range centroids {
+		for i, x := range vals {
+			denominator := 0.0
+			for _, c2 := range centroids {
+				denominator += math.Pow((x.Norm(c) / x.Norm(c2)), 2.0/(fuzziness-1.0))
+			}
+			if math.IsNaN(denominator) {
+				denominator = 1.0
+			}
+			newWeights := 1.0 / denominator
+			squareSum += math.Pow((clusterWeights[j][i] - newWeights), 2.0)
+			clusterWeights[j][i] = newWeights
+		}
+	}
+	log.Printf("epsilon: %f", math.Sqrt(squareSum))
+	return math.Sqrt(squareSum)
+}
+
+func recenter(vals []Interface, fuzziness float64, centroids []Interface, clusterWeights [][]float64) {
+
+	for i := range centroids {
+		normalization := 0.0
+		var centroid Interface
+		for j, w := range clusterWeights[i] {
+			fuzziedWeight := math.Pow(w, fuzziness)
+
+			normalization += fuzziedWeight
+
+			adder := vals[j].Multiply(fuzziedWeight)
+			if centroid == nil {
+				centroid = adder
+			} else {
+				centroid = centroid.Add(adder)
+			}
+
+		}
+		centroids[i] = centroid.Multiply(1.0 / normalization)
+
+	}
+
 }
--- fcm_test.go
+++ fcm_test.go
@@ -1,11 +1,13 @@
 package fcm
 
 import (
+	"log"
 	"math"
+	"math/rand"
 	"testing"
 )
 
-type Element int
+type Element float64
 
 func (e Element) Multiply(weight float64) Interface {
 	return Element(float64(e) * weight)
@@ -19,9 +21,19 @@ func (e Element) Norm(e2 Interface) float64 {
 
 func TestBuild(t *testing.T) {
 
-	interfaceSlice := make([]Interface, 3)
+	rand.Seed(125)
+
+	interfaceSlice := make([]Interface, 6)
 	interfaceSlice[0] = Element(2)
 	interfaceSlice[1] = Element(3)
-	interfaceSlice[2] = Element(5)
-	Cluster(interfaceSlice, 3.6, 4)
+	interfaceSlice[2] = Element(4)
+	interfaceSlice[3] = Element(5)
+	interfaceSlice[4] = Element(15)
+	interfaceSlice[5] = Element(16)
+	centroid, weights := Cluster(interfaceSlice, 2.0, 0.00001, 2)
+	for i, r := range weights {
+		for j, w := range r {
+			log.Printf("Ele %f, Cent %f, weight %f", interfaceSlice[j], centroid[i], w)
+		}
+	}
 }
